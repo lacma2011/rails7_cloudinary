a03e59588e3a883f4988e4256459cddaacb4174a
Jerome Bordallo <lacma2011@github.com>
2022-09-22 16:41:08 -0700

    more responsible way to inject environment var

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "localhost:3003" #ENV["FRONTEND_HOST"]
+     origins ENV["FRONTEND_HOST"]
 
      resource "*",
        headers: :any,
